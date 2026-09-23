53427b1c2cc942b702b4303c7f25bda832d6792d
EAG <santihidalgotaoufiki@gmail.com>
2026-09-23 16:46:34 +0200

    temario uno de acceso a datos y ejercios iniciado el 1 esta hecho el 2 no

---
+++ Acceso_a_datos/GuardarClubes.java
@@ -0,0 +1,26 @@
+package Acceso_a_datos;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class GuardarClubes {
+    public static void main(String[] args) {
+    Path ruta = Path.of("datos", "clubes.txt");
+    try {
+    Files.createDirectories(ruta.getParent());
+    try (BufferedWriter salida = Files.newBufferedWriter(
+    ruta, StandardCharsets.UTF_8)) {
+    salida.write("1;Granada CF;Granada");
+    salida.newLine();
+    salida.write("2;Málaga CF;Málaga");
+    salida.newLine();
+    salida.write("3;Cádiz CF;Cádiz");
+    salida.newLine();
+    }
+    } catch (IOException e) {
+        System.err.println("No se pudo guardar: " + e.getMessage());
+    }
+    }
+}
---
+++ Acceso_a_datos/RutasProyectos.java
@@ -0,0 +1,28 @@
+package Acceso_a_datos;
+
+import java.nio.file.Path;
+public class RutasProyectos {
+    public static void main(String[] args) {
+        Path ruta = Path.of("datos");
+        Path subruta1 = ruta.resolve("copias");
+        Path subruta2 = ruta.resolve("clubes.txt");
+        //rutas absolutas
+        System.out.println("Absoluta:");
+        System.out.println(ruta.toAbsolutePath());
+        System.out.println(subruta1.toAbsolutePath());
+        System.out.println(subruta2.toAbsolutePath());
+        //rutas relativas
+        System.out.println("Relativa:");
+        System.out.println(ruta);
+        System.out.println(subruta1);
+        System.out.println(subruta2);
+        
+        try{
+
+        }catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+
+}
